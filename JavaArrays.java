b048328d16bda4925b168e8c5b836699b24d871d
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-13 13:09:33 +0530

    pr

--- JavaArrays.java
+++ JavaArrays.java
@@ -9,5 +9,22 @@ public class JavaArrays {
         for(int b:a)
             System.out.print(b+" ");
 
+            System.out.println();
+
+        for(int i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
+
+            System.out.println();
+
+        for(int i=0;i<=4;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print("  ");
+            }
+            for(int j=i;j<=4;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
