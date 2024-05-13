591050fd39fba3a1f9dbb498499e2e9d9d5052a3
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-13 12:37:20 +0530

    try

--- JavaArrays.java
+++ JavaArrays.java
@@ -1,7 +1,13 @@
+
 public class JavaArrays {
 
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("home");
+
+        int []a={1,4,2,6,8};
+        for(int b:a)
+            System.out.print(b+" ");
+
     }
 }
