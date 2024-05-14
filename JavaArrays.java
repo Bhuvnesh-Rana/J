fc1b02e77a9dc158d9e1bb10faed9909e06cd09e
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-14 12:05:09 +0530

    W3RProp

--- JavaArrays.java
+++ JavaArrays.java
@@ -86,5 +86,13 @@ public class JavaArrays {
         }
         System.out.println("Maximum no. is: "+max);
 
+        //problem 11 reverse an array.
+        for(int i=0;i<a.length/2;i++){
+            int temp = a[i];
+            a[i]=a[a.length-i-1];
+            a[a.length-i-1]=temp;
+        }
+        System.out.println(Arrays.toString(a));
+
     }
 }
